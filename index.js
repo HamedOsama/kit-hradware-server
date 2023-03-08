eb6bef65ba1e49dd8013918d5569820c50a16326
Hamed Osama <hamedosama19@gmail.com>
2023-03-08 03:00:31 +0200

    update cors options

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 
 // 
 // app.use(cors({ credentials: true }));
-app.use(cors({ credentials: true, origin: ['http://localhost:3000' , 'https://kit-hardware-center.com','https://kit-hardware.vercel.app.com'] }));
+app.use(cors({ credentials: true, origin: ['http://localhost:3000' , 'https://kit-hardware-center.com','https://kit-hardware.vercel.app'] }));
 // app.use(cors({credentials : true , origin :  'https://hatlytest.trendlix.com'}));
 app.use(express.json());
 app.use(cookieParser());
